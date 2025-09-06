8b4595236f9c3ae5064662e356e42e7b7d1b6c2d
Zhvan14 <143933834+Zhvan14@users.noreply.github.com>
2025-09-06 22:37:53 +0300

    Update __init__.py

--- s3_interpreter/__init__.py
+++ s3_interpreter/__init__.py
@@ -1,14 +1,19 @@
 from .interpreter import S3Interpreter
 
-# Create a single instance of the interpreter when the module is imported
+# Create a single interpreter object that will be used for all calls
 _interpreter = S3Interpreter()
 
 # Define functions that use the single instance
 def interpret(code_line):
+    """
+    Runs a single line of S-language code using the single interpreter instance.
+    """
     _interpreter.interpret(code_line)
 
 def multiline(*code_lines):
+    """
+    Runs multiple lines of S-language code using the single interpreter instance.
+    """
     _interpreter.multiline(*code_lines)
 
-# The list of public items in your module
 __all__ = ['S3Interpreter', 'interpret', 'multiline']
